2e7780c0a31755d34b914a799d1312fe647f2f52
A Suicidal Cow <aaron.bertagnole@gmail.com>
2020-05-25 22:17:37 -0600

    big commit... fixed the routes as they were not working before. now trying to get my dropdown menues to populate in the survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,164 +1,63 @@
 var friendArray = [
-    {
-        name: 'George',
-        photo: 'https://images.unsplash.com/photo-1506919258185-6078bba55d2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=815&q=80',
-        score:[
-            '1',
-            '3',
-            '2',
-            '7',
-            '5',
-            '1',
-            '3',
-            '5',
-            '4',
-            '8'
-        ]
-    },
-    {
-        name: 'Lina',
-        photo: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-        score:[
-            '6',
-            '4',
-            '2',
-            '1',
-            '6',
-            '7',
-            '8',
-            '4',
-            '4',
-            '2'
-        ]
-    },
-    {
-        name: 'Damian',
-        photo: 'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-        score:[
-            '8',
-            '7',
-            '9',
-            '7',
-            '4',
-            '7',
-            '5',
-            '5',
-            '2',
-            '1'
-        ]
-    },
-    {
-        name: 'Rachel',
-        photo: 'https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80',
-        score:[
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5',
-            '5'
-        ]
-    },
-    {
-        name: 'Carly',
-        photo: 'https://unsplash.com/photos/b1Hg7QI-zcc',
-        score:[
-            '6',
-            '7',
-            '5',
-            '3',
-            '2',
-            '3',
-            '7',
-            '1',
-            '2',
-            '2'
-        ]
-    },
-    {
-        name: 'Brock',
-        photo: 'https://images.unsplash.com/photo-1489481039754-8701aeda983b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80',
-        score:[
-            '3',
-            '2',
-            '1',
-            '4',
-            '6',
-            '5',
-            '8',
-            '2',
-            '1',
-            '1'
-        ]
-    },
-    {
-        name: 'Carl',
-        photo: 'https://images.unsplash.com/photo-1530268729831-4b0b9e170218?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
-        score:[
-            '8',
-            '4',
-            '6',
-            '2',
-            '4',
-            '3',
-            '5',
-            '8',
-            '9',
-            '7'
-        ]
-    },
-    {
-        name: 'Carol',
-        photo: 'https://www.indiewire.com/wp-content/uploads/2020/04/Screen-Shot-2020-04-01-at-3.02.44-PM.png?resize=800,541',
-        score:[
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1',
-            '1'
-        ]
-    },
-    {
-        name: 'Kerri',
-        photo: 'https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-        score:[
-            '8',
-            '9',
-            '6',
-            '3',
-            '4',
-            '5',
-            '7',
-            '8',
-            '1',
-            '2'
-        ]
-    },
-    {
-        name: 'Terry',
-        photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDYwNjkzMjY3OTgz/terry-crews-headshot-600x600jpg.jpg',
-        score:[
-            '5',
-            '4',
-            '7',
-            '2',
-            '8',
-            '4',
-            '3',
-            '9',
-            '6',
-            '1'
-        ]
-    }
+  {
+    name: "George",
+    photo:
+      "https://images.unsplash.com/photo-1506919258185-6078bba55d2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=815&q=80",
+    score: ["1", "3", "2", "7", "5", "1", "3", "5", "4", "8"],
+  },
+  {
+    name: "Lina",
+    photo:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    score: ["6", "4", "2", "1", "6", "7", "8", "4", "4", "2"],
+  },
+  {
+    name: "Damian",
+    photo:
+      "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    score: ["8", "7", "9", "7", "4", "7", "5", "5", "2", "1"],
+  },
+  {
+    name: "Rachel",
+    photo:
+      "https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80",
+    score: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"],
+  },
+  {
+    name: "Carly",
+    photo: "https://unsplash.com/photos/b1Hg7QI-zcc",
+    score: ["6", "7", "5", "3", "2", "3", "7", "1", "2", "2"],
+  },
+  {
+    name: "Brock",
+    photo:
+      "https://images.unsplash.com/photo-1489481039754-8701aeda983b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80",
+    score: ["3", "2", "1", "4", "6", "5", "8", "2", "1", "1"],
+  },
+  {
+    name: "Carl",
+    photo:
+      "https://images.unsplash.com/photo-1530268729831-4b0b9e170218?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    score: ["8", "4", "6", "2", "4", "3", "5", "8", "9", "7"],
+  },
+  {
+    name: "Carol",
+    photo:
+      "https://www.indiewire.com/wp-content/uploads/2020/04/Screen-Shot-2020-04-01-at-3.02.44-PM.png?resize=800,541",
+    score: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
+  },
+  {
+    name: "Kerri",
+    photo:
+      "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    score: ["8", "9", "6", "3", "4", "5", "7", "8", "1", "2"],
+  },
+  {
+    name: "Terry",
+    photo:
+      "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE5NDg0MDYwNjkzMjY3OTgz/terry-crews-headshot-600x600jpg.jpg",
+    score: ["5", "4", "7", "2", "8", "4", "3", "9", "6", "1"],
+  },
 ];
 
 module.exports = friendArray;
--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 var express = require("express");
 var path = require("path");
+
+// Configure the Express application
+var app = express();
+var PORT = process.env.PORT || 3304;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
